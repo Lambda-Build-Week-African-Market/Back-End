9c80a74de6f1795d9ecb027e1559d0323b7bc76e
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-09-17 01:48:33 -0400

    server routers connected and test endpoints tested

--- api/server.js
+++ api/server.js
@@ -4,11 +4,17 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 
+const {registerRouter, loginRouter, itemRouter} = require('./routers/allRoutes')
+
 const server = express()
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+
+server.use('/api/register', registerRouter)
+server.use('/api/login', loginRouter)
+server.use('/api/item', itemRouter)
 
 server.get('/', (req, res, next) => {
 
