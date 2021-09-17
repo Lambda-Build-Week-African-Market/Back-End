ec123d4842551b9446c255f2fedbdeebf4f62dd1
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-09-17 01:22:50 -0400

    basic index.js and server.js built

---
+++ api/server.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+server.get('/', (req, res, next) => {
+
+    res.status(200).json({message: 'Server responsive'})
+
+})
+
+module.exports = server
---
+++ index.js
@@ -0,0 +1,10 @@
+
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 3333
+
+server.listen(PORT, () => {
+
+    console.log(`-+* Server online on port ${PORT}`)
+
+})
