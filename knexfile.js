343c0f4dbe42ec29bf7cb3237f0fdcac3de6742b
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-09-19 05:26:49 -0400

    edited knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,60 @@
 // Update with your config settings.
 
+const baseConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations'
+  },
+  pool: {
+    afterCreate: (connect, done) => connect.run('PRAGMA foreign_keys = ON', done)
+  }
+}
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    ...baseConfig,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/marketDB.db3'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // COMMENTED OUT UNTIL ON HEROKU
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  // COMMENTED OUT UNTIL FINISHED
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
