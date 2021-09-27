dac60146016b70a10c78c42031ad3247c6eb578d
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-09-26 22:57:18 -0400

    more bugfixes

--- api/middleware/items/authorized.js
+++ api/middleware/items/authorized.js
@@ -7,6 +7,8 @@ const authorized = (req, res, next) => {
 
     const token = req.headers.authorization
 
+    console.log(token)
+
     if (!token) {
 
         res.status(401).json({message: 'You are not authorized to view this resource'})
@@ -16,6 +18,7 @@ const authorized = (req, res, next) => {
         jwt.verify(token, SECRET, (err, decoded) => {
 
             if (err) {
+                console.log(err)
                 res.status(401).json({message: 'Token is invalid'})
             } else {
                 next()
